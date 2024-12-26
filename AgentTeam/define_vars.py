b9971839e958bbce68d0326fabd6dabe433c92a7
Kaiden Simon <69560455+485585r@users.noreply.github.com>
2024-12-26 12:06:45 -0800

    Update define_vars.py

--- AgentTeam/define_vars.py
+++ AgentTeam/define_vars.py
@@ -10,12 +10,12 @@ def set_environment_variables(project_name: str = "") -> None:
 
 
 
-    os.environ["OPENAI_API_KEY"] = str(config("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",
-                                              default="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"))
-    os.environ["LANGCHAIN_API_KEY"] = str(config("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",
-                                                 default="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"))
+    os.environ["OPENAI_API_KEY"] = str(config("",
+                                              default=""))
+    os.environ["LANGCHAIN_API_KEY"] = str(config("",
+                                                 default=""))
     os.environ["LANGCHAIN_PROJECT"] = project_name
     os.environ['TAVILY_API_KEY'] = str(
-        config("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", default="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"))
+        config("", default=""))
 
     print("API KEYS LOAED AND TRACING SET WITH PROJECT NAME", project_name)
